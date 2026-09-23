0441d86963fec0af5c899142cb1c57ba6ef66fb7
Aram Valcourt <johnnyvalcourt@yahoo.com>
2026-09-22 20:09:12 -0400

    fixed alignment initialization issue

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ import express from "express";
 //Route Imports
 import mealsRoutes from "./routes/mealsRoutes.js";
 
-// API Routes
-app.use("/meals", mealsRoutes);
-
 const app = express();
 const port = 3000;
+
+// API Routes
+app.use("/meals", mealsRoutes);
 
 app.listen(port, () => {
 	console.log(`Server is running on PORT http://localhost:${port}`);
